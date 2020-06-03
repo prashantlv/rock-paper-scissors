0360935dd116a66456c4616790a7b247a767d7ce
playground <=>
2020-06-03 12:15:53 +0530

    Change InputLayer

--- model.py
+++ model.py
@@ -1,12 +1,12 @@
 import tensorflow as tf 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Activation
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Activation, InputLayer
 from tensorflow.keras.optimizers import Adam
 
 def get_model(temp):
     temp = temp[1:]
     model = Sequential()
-    model.add(Dense(128, input_shape=(300, 300, 3)))
+    model.add(InputLayer(input_shape=(300, 300, 3)))
     model.add(Conv2D(128, (5,5), activation='relu'))
     model.add(Conv2D(128, (5,5), activation='relu'))
     model.add(MaxPooling2D(pool_size=(3,3)))
@@ -27,7 +27,7 @@ def get_model(temp):
     model.add(Flatten())
     model.add(Dense(64))
     model.add(Activation('relu'))
-    model.add(Dense(3))
+    model.add(Dense(4   ))
     model.add(Activation('softmax')) 
     
     return model
